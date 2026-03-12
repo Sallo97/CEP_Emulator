67f55755db16331cc9c0e43f49dd28e81da615b8
Sal <salerno.salvatore1997@gmail.com>
2026-03-12 20:17:00 +0100

    Delete test_simple_circuits.zig

--- src/test/simple_circuits/test_simple_circuits.zig
+++
@@ -1,9 +0,0 @@
-//! Global test file containing all tests for basic circuits.
-
-const std = @import("std");
-pub const test_adder_f = @import("test_adder.zig");
-pub const test_register_f = @import("test_register.zig");
-
-test {
-    std.testing.refAllDecls(@This());
-}
